e400551a782cb444e0c921ff98a47ec9f1f74816
R.S. Knox <rsknox@gmail.com>
2020-07-18 13:40:25 -0500

    corrected code in Posn_from_circles to produce correct posn

--- Robot/RPiCode/Posn_from_circles.py
+++ Robot/RPiCode/Posn_from_circles.py
@@ -8,8 +8,8 @@ Bx, By = 50, 76  # center of cirlce B
 Cx, Cy = 100, 76  # center of circle C 
 RA, RB, RC = 83, 52, 62 # radius of circles (range from robot)
 
-tagc = [(0,76),(50,76),(100,76)]
-rng = [83,52,62]
+tagc = [(0,76),(50,76),(100,76),(100,36)]
+rng = [83,52,62,36]
 
 D = []
 delta = []
@@ -31,46 +31,50 @@ for i in range(len(tagc)):
         D.append(math.sqrt(((tagc[i][0] - tagc[j][0])**2) + (tagc[i][1] - tagc[j][1])**2))
         print('\n',"List of d's: ",D)
 
-c1 = 0
-c2 = 0
-ln=len(D)-1
-for i in range(ln):
-    c1 = i
-    print('\n','i-loop: ', c1)
+#c1 = 0
+#c2 = 0
+#calculate delta's
+k=0
+for i in range(len(tagc)):
     for j in range (i+1, len(tagc)):
-        c2 = j
-        print('\n','i-loop, j-loop: ',c1,c2)
-        df1 = D[j-i-1]+rng[i]+rng[j]
-        df2 = D[j-i-1]+rng[i]-rng[j]
-        df3 = D[j-i-1]-rng[i]+rng[j]
-        df4 = -D[j-i-1]+rng[i]+rng[j]
-        print('\n','D[j-i], rng[i], rng[j]: ', D[j-i-1], rng[i], rng[j])
+        df1 = D[k]+rng[i]+rng[j]
+        df2 = D[k]+rng[i]-rng[j]
+        df3 = D[k]-rng[i]+rng[j]
+        df4 = -D[k]+rng[i]+rng[j]
+        print('\n','D[k], rng[i], rng[j]: ', D[k], rng[i], rng[j])
         print('\n', 'dfactors: ', df1, df2, df3, df4)
         delta.append(0.25*math.sqrt(df1*df2*df3*df4))
-        print('\n','list of deltas: ', delta, '\n')
-        f1 = (tagc[i][0] + tagc[j][0])/2
-        print('\n','i:',i,'j:',j,' f1: ', f1, ' tagc[i]:',tagc[i][0],' tagc[j]:',tagc[j][0])
-        f2 = (tagc[j][0] - tagc[i][0])*(rng[i]**2 - rng[j]**2)/(2*D[j-i-1]**2)
-        print('\n','i:',i,'j:',j,'f2: ',f2)
-        print('tagc[j]:',tagc[j][0],'tagc[i]:',tagc[i][0],'rng[i]:',rng[i],'rng[j]',rng[j],'D[j-i-1]:',D[j-i-1])
-        f3 = (2*(tagc[i][1]-tagc[j][1])/D[j-i-1]**2)*delta[j-i-1]
-        print('\n','i:',i,'j:',j,'f3: ',f3)
-        print('tagc[i]:',tagc[i][1],'tagc[j]:',tagc[j][1],'D[j-i-1]:',D[j-i-1],'delta[j-i-1]:',delta[j-i-1])
+        k=k+1
+        
+print('\n','list of deltas: ', delta, '\n')
+
+k=0
+for i in range(len(tagc)):
+    for j in range(i+1,len(tagc)):
+        
+        f1 = round((tagc[i][0] + tagc[j][0])/2,2)
+        print('\nf1: ', f1, ' tagc[i]:',tagc[i][0],' tagc[j]:',tagc[j][0])
+        f2 = round((tagc[j][0] - tagc[i][0])*(rng[i]**2 - rng[j]**2)/(2*D[k]**2),2)
+        print('f2: ',f2,'tagc[j]:',tagc[j][0],'tagc[i]:',tagc[i][0],'rng[i]:',rng[i],
+              'rng[j]',rng[j],'D[k]:',D[k])
+        f3 = round((2*(tagc[i][1]-tagc[j][1])/D[k]**2)*delta[k],2)
+        print('f3: ',f3, 'tagc[i]:',tagc[i][1],'tagc[j]:',
+              tagc[j][1],'D[k]:',D[k],'delta[k]:',delta[k])
         x1.append(f1 + f2 + f3)           
-        print('\n','x1: ', x1)
+        print('\nx1: ', x1)
         x2.append(f1 + f2 - f3)
         print('x2: ', x2)
-        g1 = (tagc[i][1] + tagc[j][1])/2
-        print('\n','g1: ', g1)
-        g2 = (tagc[j][1] - tagc[i][1])*(rng[1]**2 - rng[j]**2)/(2*D[j-i-1]**2)
-        print('\n','g2: ',g2)
-        g3 = (2*(tagc[i][0]-tagc[j][0])/(D[j-i-1]**2))*delta[j-i-1]
-        print('\n','g3: ',g3,' tagc[i]: ',tagc[i][0], ' tagc[j]: ', tagc[j][0], ' D[j-i-1]: ', D[j-i-1], ' delta: ',delta[j-i-1])
+        g1 = round((tagc[i][1] + tagc[j][1])/2,2)
+        print('\ng1: ', g1)
+        g2 = round((tagc[j][1] - tagc[i][1])*(rng[i]**2 - rng[j]**2)/(2*D[k]**2),2)
+        print('g2: ',g2)
+        g3 = round((2*(tagc[i][0]-tagc[j][0])/(D[k]**2))*delta[k],2)
+        print('g3: ',g3,' tagc[i]: ',tagc[i][0], ' tagc[j]: ', tagc[j][0], ' D[k]: ', D[k], ' delta: ',delta[k])
         y1.append(g1 + g2 -g3)
-        print('\n','y1: ', y1)
+        print('\ny1: ', y1)
         y2.append(g1 + g2 +g3)
         print('y2: ', y2)
-        
+        k=k+1
        
            
 #    print('i: ', tagc[ln])
