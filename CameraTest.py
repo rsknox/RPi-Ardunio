674785657b04f161fb48f9ae1ca330d25aa65cd6
R.S. Knox <rsknox@gmail.com>
2020-08-08 06:28:01 -0900

    added CameraTest.py

---
+++ CameraTest.py
@@ -0,0 +1,48 @@
+# Camera Test - test operation of cameras on RPi Zero
+# first execute cmd 'raspistill -o imgage.jpg' to test the camera to Zero connection
+
+from picamera import PiCamera
+from time import sleep
+import datetime
+#import cv2
+import math
+import time
+import glob
+import os
+import sys
+import signal
+import logging
+
+def signal_handler(sig, frame):
+    print('ctrl-c detected')
+    logging.info('ctrl-c detected')
+    sys.exit(0)
+
+def i_capture(name):
+    file_name = "/home/pi/Git/RPi-Ardunio/" + name + datetime.datetime.now().strftime("%Y%m%d-%H%M%S.jpg")
+    camera.capture(file_name)
+    print('\n', file_name)
+    logging.info('File name: {a}'.format (a=file_name))
+    
+logging.basicConfig(filename='CameraTest.log', level=logging.INFO, format='%(asctime)s %(message)s')
+signal.signal(signal.SIGINT, signal_handler)
+
+
+camera = PiCamera()
+camera.resolution = (1280, 1024)
+
+logging.info('Start camera')
+
+camera.start_preview(fullscreen=False, window=(700,100,512,384))
+sleep(2)  # give camera time to stablize
+
+for i in range(10):
+    i_capture('Camera_Zx')
+    
+#     list_files = glob.glob('/home/pi/Git/RPi-Ardunio/*.jpg')
+#     cal_image = max(list_files, key=os.path.getctime)
+#     print ("CameraZx image", cal_image)
+#     logging.info('CameraZx image file: {a}'.format (a=cal_image))
+               
+camera.stop_preview()
+sys.exit()
