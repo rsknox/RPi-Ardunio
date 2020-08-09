41a2466823afdce5a3a5beebb7696f0fa11168ab
R.S <rsknox@gmail.com>
2020-08-09 11:13:21 -0500

    add everything

---
+++ FileTransfer/Client/cookieClient2.py
@@ -0,0 +1,26 @@
+import socket
+from time import sleep
+
+host = '192.168.1.79'
+port = 5560
+
+def setupSocket():
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.connect((host, port))
+    return s
+
+def sendReceive(s, message):
+    s.send(str.encode(message))
+    reply = s.recv(1024)
+    print("We have received a reply")
+    print("Send closing message.")
+    s.send(str.encode("EXIT"))
+    s.close()
+    reply = reply.decode('utf-8')
+    return reply
+
+def transmit(message):
+    s = setupSocket()
+    response = sendReceive(s, message)
+    return response
+
